b74f772f0a8c56ded6165dc27f637577c54514c9
Shilpa332 <gvshilpa121@gmail.com>
2024-12-18 10:29:51 +0530

    staged

--- app1/urls.py
+++ app1/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import *
+from app1.views import page1, book_info, employee_info, forms_info
 
 
-urlpatterns=[
-    path('',page1),
-    path('book_info',book_info),
-    path('employee_info',employee_info),
-    path('forms_info',forms_info),
+urlpatterns = [
+    path("", page1),
+    path("book_info", book_info),
+    path("employee_info", employee_info),
+    path("forms_info", forms_info),
 ]
